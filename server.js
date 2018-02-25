e646c921bf4b28633c0a01ed1b8e20d4a92b6a39
Satyaveer Krovvidi <knvrsatyaveer@gmail..com>
2018-02-25 11:41:38 +1000

    setup start script and heroku port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.set('view engine','hbs');
@@ -67,4 +67,6 @@ app.get('/about1',(req,resp) =>{
 app.get('/bad',(req,resp) =>{
    resp.send(' <h1> bad request </h1>');
 });
-app.listen(3000);
+app.listen(port , () => {
+    console.log(`server is up for port ${port}`);
+});
